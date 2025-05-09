dad256538df8f1c79d539053ea0e8231f866179f
Stanislaw Malinowski <stanislawmalinowski09@gmail.com>
2025-05-09 17:43:18 +0000

    rmq stomp connected after plugins comma formatting

--- verify/stomptest.py
+++ verify/stomptest.py
@@ -12,9 +12,9 @@ class MyListener(stomp.ConnectionListener):
         print(f'received a message "{frame.body}"')
 
 
-conn = stomp.Connection()
+conn = stomp.Connection([("rmq", 61613)], auto_content_length=False)
 conn.set_listener("", MyListener())
-conn.connect("user2", "password", wait=True)
+conn.connect("user", "password", wait=True)
 conn.subscribe(destination="/queue/test", id=1, ack="auto")
 conn.send(body=" ".join(sys.argv[1:]), destination="/queue/test")
 time.sleep(2)
