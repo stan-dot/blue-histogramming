ba49dd88e4469d0542d182bd7a90def660c7f7c5
Stanislaw Malinowski <stanislawmalinowski09@gmail.com>
2025-05-14 13:50:40 +0100

    fix double connect in fake data stream

--- fake_stream/fake_data_stream.py
+++ fake_stream/fake_data_stream.py
@@ -7,7 +7,7 @@ import stomp
 CHANNEL = "/topic/public.worker.event"
 
 
-class STOMPListener(stomp.ConnectionListener):
+class STOMPListener(stomp.PrintingListener):
     def on_error(self, frame):
         print(f"Error: {frame.body}")
 
@@ -20,22 +20,43 @@ class STOMPListener(stomp.ConnectionListener):
 def start_stomp_connection():
     conn = stomp.Connection([("rmq", 61613)], auto_content_length=False)
     conn.set_listener("", STOMPListener())
-    # conn.connect("user", "password", wait=True)
     conn.connect("user", "password", wait=True)
-    conn.connect(wait=True)
     return conn
 
 
 async def replay_events_from_json(
     json_path: Path, conn: stomp.Connection, delay: float = 0.05
 ):
+    """
+    Replay events from a JSON file to a STOMP connection.
+    Args:
+        json_path (Path): Path to the JSON file containing events.
+        conn (stomp.Connection): STOMP connection object.
+        delay (float): Delay between sending events in seconds.
+    """
+    if not json_path.exists():
+        print(f"File {json_path} does not exist.")
+        return
+    if not json_path.is_file():
+        print(f"{json_path} is not a file.")
+        return
+    if not json_path.suffix == ".json":
+        print(f"{json_path} is not a JSON file.")
+        return
+    print(f"Reading events from {json_path}")
     with open(json_path) as f:
         data = json.load(f)
+    length = len(data)
+    print(f"Loaded {length} events from {json_path}")  
+    if length == 0:
+        print(f"No events found in {json_path}")
+        return
 
     events = data.get("events", [])
     for event in events:
         print(f"🔁 Emitting: {event['name']} - {event.get('doc', {}).get('uid', '')}")
         message = json.dumps(event)
+        print(f"🔁 tring to send message: {message}")
         conn.send(destination=CHANNEL, body=message)
         print(f"🔁 Sent event to STOMP: {message}")
         await asyncio.sleep(delay)
@@ -43,7 +64,8 @@ async def replay_events_from_json(
 
 async def main():
     conn = start_stomp_connection()
-    await replay_events_from_json(Path("events-to-emit-6.json"), conn)
+    print("Connected to STOMP broker")
+    await replay_events_from_json(Path("events-to-emit-6.json"), conn, 2)
     conn.disconnect()
 
 
